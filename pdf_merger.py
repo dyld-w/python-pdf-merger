fbe706887a506049b802607cbaca9f6681e5e5d5
dyld-w <dd4cnj@virginia.edu>
2021-01-13 16:52:12 -0500

    improves file type checking

--- pdf_merger.py
+++ pdf_merger.py
@@ -5,20 +5,12 @@ from pikepdf import Pdf
 
 
 def merger():
-    # input path to folder in command line
-    #  num_args = len(arguments)
-    #  if num_args =
-    #  directory = arguments[1]
-    #  new_name = arguments[2]
-    #  print(directory)
-    # https://stackoverflow.com/questions/3964681/find-all-files-in-a-directory-with-extension-txt-in-python
 
     valid_directory = False
 
     while not valid_directory:
-        directory = input(
-            "Path to directory with the PDFs to be merged or 'cwd' for your current working directory: "
-        )
+        directory = input("Path to directory with the PDFs to be merged "
+                          "or 'cwd' for your current working directory: ")
 
         if directory == "cwd":
             directory = os.getcwd()
@@ -28,33 +20,42 @@ def merger():
         else:
             print("Invalid input. Directory not found.")
 
-    new_name = input("Name for the merged PDF: ")
+    print("Directory found:", directory)
 
     if directory[len(directory) - 1] != "/":
         directory += "/"
 
-    print(directory)
+    dir_list = os.listdir(directory)
+    pdf_list = []
 
-    if os.path.isdir(directory):
-        print("Directory found. Merging .pdf files")
-        dir_list = os.listdir(directory)
-        dir_list.sort()
-        pdf = Pdf.new()
-        version = pdf.pdf_version
+    for file in dir_list:
+        name, ext = os.path.splitext(file)
 
-        for file in dir_list:
-            if file.endswith(".pdf"):
-                print("appending", file)
-                inp_PDF = Pdf.open(directory + file)
-                version = max(version, inp_PDF.pdf_version)
-                pdf.pages.extend(inp_PDF.pages)
+        if ext == '.pdf':
+            pdf_list.append(file)
+
+    pdf_list.sort()
+    len_pdf_list = len(pdf_list)
+    pdf = Pdf.new()
+    version = pdf.pdf_version
+
+    if len_pdf_list < 2:
+        print("Only", len_pdf_list,
+              ".pdf files in this directory. Not merging")
+    else:
+        new_name = input("Name for the merged PDF: ")
+        print("Merging", len_pdf_list,
+              "files into a file called " + new_name + ".pdf")
+
+        for file in pdf_list:
+            print("appending", file)
+            inp_PDF = Pdf.open(directory + file)
+            version = max(version, inp_PDF.pdf_version)
+            pdf.pages.extend(inp_PDF.pages)
         pdf.remove_unreferenced_resources()
         pdf.save(directory + new_name + ".pdf", min_version=version)
         print("Merged pdf should now be in " + directory)
-    else:
-        print("Directory not found")
 
 
 if __name__ == "__main__":
-    #  print(len(sys.argv))
     merger()
